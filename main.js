42014bf4a41612841a49cd3a716c315fc83f9f31
Nathan Dunn <dunn.m.nathan@gmail.com>
2019-11-03 04:19:00 -0500

    Incomplete Project of November 3th 2019

---
+++ main.js
@@ -0,0 +1,18 @@
+// This code was used for testing images
+
+
+// const project2 = document.querySelector("#project-2");
+// let animation = [
+//     {transform: 'translate(-100%)'},
+//     {transform: 'translate(0)'}
+// ]
+// let animatetime = {
+//     duration: 500,
+//     iterations: 1
+// }
+// project2.addEventListener("mouseout",function(){
+//     project2.animate(
+//         animation,
+//         animatetime
+//     )
+//     })
